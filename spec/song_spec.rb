305e3e2dff1cf1e154ff0d3283ae9aa8076e8913
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-19 23:34:10 +0000

    Done.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -24,17 +24,10 @@ describe 'Song' do
   end
 
   describe '.new_by_filename' do
-    it 'creates a new instance of a song from the file that\'s passed' do
+    it 'creates a new instance of a song from the file that\'s passed in' do
       new_instance = Song.new_by_filename(file_name)
       expect(new_instance.name).to eq('Black or White')
-    end
-
-    it 'associates new song instance with the artist from the filename' do
-      Artist.class_variable_set("@@all",[])
-      new_instance = Song.new_by_filename(file_name)
       expect(new_instance.artist.name).to eq('Michael Jackson')
-      expect(Artist.all.size).to eq(1)
-      expect(Artist.all.first.songs.empty?).to eq(false)
     end
   end
 end
